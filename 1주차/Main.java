3339993648e2620e2421e4888b208e6e08bf04cb
03jiho <105620249+03jiho@users.noreply.github.com>
2026-03-29 16:44:49 +0900

    add : 김지호 1주차 과제

--- 1주차/Main.java
+++ 1주차/Main.java
@@ -1,29 +1,30 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("저장할 아기 사자 수를 5 이상 입력해주세요");
-        int repeat = sc.nextInt();
-
-        
-        while (repeat < 5) {
-            System.out.println("[오류] 5 이상 입력해주세요");
-            System.out.println("5 이상 입력해주세요");
+        int repeat;
+        while (true) {
+            System.out.println("저장할 아기사자 수를 5 이상 입력해주세요.");
             repeat = sc.nextInt();
+
+            if (repeat >= 5) {
+                break;
+            } else {
+                System.out.println("[오류] 5 이상 입력해주세요.");
+            }
         }
 
-        
         String[] babyLions = new String[repeat];
-
-        System.out.println("아기 사자들의 이름을 입력해주세요");
+        
+        System.out.println("아기사자 이름을 입력해주세요.");
         for (int i = 0; i < repeat; i++) {
             babyLions[i] = sc.next();
         }
 
-        System.out.println("\n아기 사자 명단을 출력합니다.");
-        for (int i = 0; i < repeat; i++) {
+        System.out.println("\n아기사자 명단을 최종적으로 출력합니다.");
+        for (int i = 0; i < babyLions.length; i++) {
             System.out.println((i + 1) + ". " + babyLions[i]);
         }
 
