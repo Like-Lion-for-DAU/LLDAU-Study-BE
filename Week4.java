f609901a8c3666fb89dc4764ba8f692912a99559
SEOUIJIN <247989814+drg20070308-netizen@users.noreply.github.com>
2026-05-06 17:53:28 +0900

    수정하였음

--- Week4.java
+++ Week4.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Scanner; // 사용자에게 값을 입력받기 위해 Scanner 기능을 가져옴
+import java.util.Scanner;
 
 public class Week4 {
     public static void main(String[] args) {
@@ -8,33 +8,50 @@ public class Week4 {
         System.out.print("멤버의 수는? ");
         int j = sc.nextInt();
         ArrayList<ArrayList<String>> list = new ArrayList<>();
-        ArrayList<String> member = new ArrayList<>();
         for (int i = 0; i < j ; i++) {
-            System.out.print("이름을 입력하세요: ");
-            String name = sc.nextLine();
-            System.out.print("학번을 입력하세요: ");
-            String studentId = sc.nextLine();
-            System.out.print("파트를 입력하세요: ");
-            String part = sc.nextLine();
-            member.add(name);
-            member.add(studentId);
-            member.add(part);
-            list.add(member);
-            System.out.println(list);
-            //멤버 수만큼 리스트를 만들어서 넣을 수 있게 해야하는데 못하겠습니다.
-           //member값 list에 저장시키고 비워도 list에 저장된 값이 사라지는 문제가 있습니다. 어떻게 해야할까요? 
+            ArrayList<String> Row = new ArrayList<>(); // 멤버 리스트 초기화
+            
+                sc.nextLine(); // 버퍼 비우기
+                System.out.print("이름을 입력하세요: ");
+                String name = sc.nextLine();
+
+                System.out.print("학번을 입력하세요: ");
+                String studentId = sc.nextLine();
+
+
+                
+                System.out.print("파트를 입력하세요: ");
+                String part = sc.nextLine();
+            Row.add(name);
+            Row.add(studentId);
+
+            Row.add(part);
+            
+        list.add(Row);
         
     }
+            System.out.println(list);
+
         System.out.print("파트를 입력하세요: ");
         String part = sc.nextLine();
         if (list.contains(part)) {
-            System.out.println(member);
-        } 
+            //System.out.println(list);
+        
     HashMap map = new HashMap<>();
-    if (list.contains("백엔드")) {
-        map.put("백엔드", member);
+   
+        for (ArrayList<String> Row : list) {
+            String partKey = Row.get(2); // 파트 정보는 Row의 2번째 요소에 저장되어 있다고 가정
+           map.put(partKey, Row); // 파트를 키로, 전체 Row를 값으로 저장
+            }
+        
+        System.out.println(map.get("백엔드"));
+        for (String Key : map.keySet()) {//왜 오류가 나나요
+    System.out.println("키(이름): " + Key + " / 값(데이터): " + map.get(Key));
+        }
+    }
+}
+}
+//
 
-}
-}
-}
-//member들 개수만 어떻게 좀 자동적으로 늘릴 수 없을까요
+
+//이중for문 했다가 바꿔서 그냥 ArrayList<ArrayList<String>> list = new ArrayList<>(); 하니까 됩니다. 왜 되나요?
